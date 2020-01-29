a56d65efbab968b60937daa3e111f5b05fc10285
niels <ashomega@live.fr>
2020-01-29 05:07:51 +0100

    wdedwef

--- ft_printf/srcs/flag_u.c
+++ ft_printf/srcs/flag_u.c
@@ -6,7 +6,7 @@
 /*   By: nisauvig <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/03 19:22:24 by nisauvig          #+#    #+#             */
-/*   Updated: 2020/01/25 09:01:25 by nisauvig         ###   ########.fr       */
+/*   Updated: 2020/01/29 02:47:44 by nisauvig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,24 @@
 int		flag_u(va_list arg, t_flag *flags, t_buffer *save)
 {
 	unsigned int	i;
-	char				*res;
+	char			*tmp;
+	char			*res;
 
 	i = va_arg(arg, unsigned int);
 	if (flags->plus || flags->space)
 		return (ERROR);
-	res = ft_utoa_base(i, 10);
-	flags->precision != ERROR ? res = app_precision(res, flags, 'u'): 0;
-	flags->width != ERROR ? res = app_width(res, 0, flags): 0;
-	ft_putstr_buffer(res, save);
+	if (i == 0 && (flags->precision == 0 || (flags->point == TRUE
+					&& flags->precision == ERROR)))
+		res = ft_strdup(" ");
+	else
+		res = ft_utoa_base(i, 10);
+	if (flags->precision != ERROR ? tmp = app_precision(res, flags, 'u') : 0)
+	{
+		free(res);
+		res = tmp;
+	}
+	if (flags->width != ERROR ? tmp = app_width(res, 0, flags) : 0)
+		free(res);
+	tmp ? ft_putstr_buffer(tmp, save) : ft_putstr_buffer(res, save);
 	return (1);
 }
